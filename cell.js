b50ef9e36dda8b4f4914557ec76efa06eed3c900
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-09-11 20:29:55 +0530

    Added beautification

--- cell.js
+++ cell.js
@@ -1,74 +1,68 @@
 class Cell {
 
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.neighboursBeesCount = 0;
-      this.bee = false;
-      this.revealed = false;
-    }
-  
-    countBees() {
-      let total = 0;
-      if (!this.bee) {
-        for (let i = -1; i <= 1; i++) {
-          for (let j = -1; j <= 1; j++) {
-            // console.log("Working");
-            let xoff = this.x + i;
-            let yoff = this.y + j;
-            if (xoff > -1 && xoff < cols && yoff > -1 && yoff < rows) {
-              let neighbour = cells[(xoff * cols) + yoff];
-              if (neighbour.bee)
-                total++;
-            }
-          }
-        }
-      }
-      else
-        total=-1;
-      this.neighboursBeesCount = total;
-    }
-  
-    reveal() {
-      this.revealed = true;
-      document.getElementById(String((this.x * cols) + this.y)).classList.add("revealed");
-      if(this.bee){
-        document.getElementById(String((this.x * cols) + this.y)).classList.add("bee");
-        // revealAll();
-      }
-      else{
-        if(this.neighboursBeesCount === 0){
-        }
-        else if(this.neighboursBeesCount === 1){
-          document.getElementById(String((this.x * cols) + this.y)).innerText = this.neighboursBeesCount;
-          document.getElementById(String((this.x * cols) + this.y)).classList.add("blue");
-        }
-        else if(this.neighboursBeesCount === 2){
-          document.getElementById(String((this.x * cols) + this.y)).innerText = this.neighboursBeesCount;
-          document.getElementById(String((this.x * cols) + this.y)).classList.add("green");
-        }
-        else if(this.neighboursBeesCount >= 3){
-          document.getElementById(String((this.x * cols) + this.y)).innerText = this.neighboursBeesCount;
-          document.getElementById(String((this.x * cols) + this.y)).classList.add("red");
-        }
-      }
-      if (this.neighboursBeesCount == 0)
-        this.floodfill();
-    }
-  
-    floodfill() {
-      for (let i = -1; i <= 1; i++) {
-          for (let j = -1; j <= 1; j++) {
-            // console.log("Working");
-            let xoff = this.x + i;
-            let yoff = this.y + j;
-            if (xoff > -1 && xoff < cols && yoff > -1 && yoff < rows) {
-              let neighbour = cells[(xoff * cols) + yoff];
-              if (!neighbour.bee && !neighbour.revealed)
-                neighbour.reveal();
-            }
-          }
-        }
-    }
-  
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.neighboursBeesCount = 0;
+    this.bee = false;
+    this.revealed = false;
   }
+
+  countBees() {
+    let total = 0;
+    if (!this.bee) {
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          // console.log("Working");
+          let xoff = this.x + i;
+          let yoff = this.y + j;
+          if (xoff > -1 && xoff < cols && yoff > -1 && yoff < rows) {
+            let neighbour = cells[(xoff * cols) + yoff];
+            if (neighbour.bee)
+              total++;
+          }
+        }
+      }
+    } else
+      total = -1;
+    this.neighboursBeesCount = total;
+  }
+
+  reveal() {
+    this.revealed = true;
+    document.getElementById(String((this.x * cols) + this.y)).classList.add("revealed");
+    if (this.bee) {
+      document.getElementById(String((this.x * cols) + this.y)).classList.add("bee");
+      // revealAll();
+    } else {
+      if (this.neighboursBeesCount === 0) {} else if (this.neighboursBeesCount === 1) {
+        document.getElementById(String((this.x * cols) + this.y)).innerText = this.neighboursBeesCount;
+        document.getElementById(String((this.x * cols) + this.y)).classList.add("blue");
+      } else if (this.neighboursBeesCount === 2) {
+        document.getElementById(String((this.x * cols) + this.y)).innerText = this.neighboursBeesCount;
+        document.getElementById(String((this.x * cols) + this.y)).classList.add("green");
+      } else if (this.neighboursBeesCount >= 3) {
+        document.getElementById(String((this.x * cols) + this.y)).innerText = this.neighboursBeesCount;
+        document.getElementById(String((this.x * cols) + this.y)).classList.add("red");
+      }
+    }
+    if (this.neighboursBeesCount == 0)
+      this.floodfill();
+  }
+
+  floodfill() {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        // console.log("Working");
+        let xoff = this.x + i;
+        let yoff = this.y + j;
+        if (xoff > -1 && xoff < cols && yoff > -1 && yoff < rows) {
+          let neighbour = cells[(xoff * cols) + yoff];
+          if (!neighbour.bee && !neighbour.revealed)
+            neighbour.reveal();
+        }
+      }
+    }
+  }
+
+}
--- scripts.js
+++ scripts.js
@@ -1,51 +1,51 @@
 var node = document.getElementById("container");
 
-var cols = 10,rows = 10;
+var cols = 10,
+  rows = 10;
 var options = [];
 var beesCount = 20;
 var cells = [];
 
-for(let i = 0;i<rows;i++){
-    for(let j=0;j<cols;j++){
-        let button = document.createElement('button');
-        button.setAttribute('class','button');
-        button.setAttribute('id', String(i*cols + j));
-        button.setAttribute('onclick', "buttonReveal(this)")
-        node.appendChild(button);
-        options.push([i,j]);
-        cells.push(new Cell(i,j));
-    }
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < cols; j++) {
+    let button = document.createElement('button');
+    button.setAttribute('class', 'button');
+    button.setAttribute('id', String(i * cols + j));
+    button.setAttribute('onclick', "buttonReveal(this)")
+    node.appendChild(button);
+    options.push([i, j]);
+    cells.push(new Cell(i, j));
+  }
 }
 
 for (let i = 0; i < beesCount; i++) {
-    let index = Math.floor(Math.random()*options.length);
-    let point = options[index];
-    let x = point[0];
-    let y = point[1];
-    options.splice(index,1);
-    cells[(x*cols)+y].bee = true;
+  let index = Math.floor(Math.random() * options.length);
+  let point = options[index];
+  let x = point[0];
+  let y = point[1];
+  options.splice(index, 1);
+  cells[(x * cols) + y].bee = true;
 }
 
 for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < cols; j++) {
+    cells[(i * cols) + j].countBees();
+  }
+}
+
+function buttonReveal(item) {
+  if (cells[parseInt(item.id)].bee) {
+    document.getElementById(item.id).classList.add("attacked");
+    revealAll();
+  } else
+    cells[parseInt(item.id)].reveal();
+}
+
+
+function revealAll() {
+  for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      cells[(i * cols) + j].countBees();
+      cells[(i * cols) + j].reveal();
     }
   }
-
-  function buttonReveal(item){
-    if(cells[parseInt(item.id)].bee){
-      document.getElementById(item.id).classList.add("attacked");
-      revealAll();
-    }
-    else
-      cells[parseInt(item.id)].reveal();
-  }
-
-
-function revealAll(){
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          cells[(i * cols) + j].reveal();
-        }
-      }
 }
