5feab8ec14d9b39ee8b80d0a7e254818e54f40b9
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-09-12 21:31:06 +0530

    Added restart functionalities

--- scripts.js
+++ scripts.js
@@ -5,40 +5,52 @@ var cols = 10,
 var options = [];
 var beesCount = 20;
 var cells = [];
+var gameover = false;
 
-for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < cols; j++) {
-    let button = document.createElement('button');
-    button.setAttribute('class', 'button');
-    button.setAttribute('id', String(i * cols + j));
-    button.setAttribute('onclick', "buttonReveal(this)")
-    node.appendChild(button);
-    options.push([i, j]);
-    cells.push(new Cell(i, j));
+function setup() {
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let button = document.createElement('button');
+      button.setAttribute('class', 'button');
+      button.setAttribute('id', String(i * cols + j));
+      button.setAttribute('onclick', "buttonReveal(this)")
+      node.appendChild(button);
+      options.push([i, j]);
+      cells.push(new Cell(i, j));
+    }
   }
-}
 
-for (let i = 0; i < beesCount; i++) {
-  let index = Math.floor(Math.random() * options.length);
-  let point = options[index];
-  let x = point[0];
-  let y = point[1];
-  options.splice(index, 1);
-  cells[(x * cols) + y].bee = true;
-}
-
-for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < cols; j++) {
-    cells[(i * cols) + j].countBees();
+  for (let i = 0; i < beesCount; i++) {
+    let index = Math.floor(Math.random() * options.length);
+    let point = options[index];
+    let x = point[0];
+    let y = point[1];
+    options.splice(index, 1);
+    cells[(x * cols) + y].bee = true;
   }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      cells[(i * cols) + j].countBees();
+    }
+  }
+
 }
+
+setup();
+
 
 function buttonReveal(item) {
-  if (cells[parseInt(item.id)].bee) {
-    document.getElementById(item.id).classList.add("attacked");
-    revealAll();
-  } else
-    cells[parseInt(item.id)].reveal();
+  if (!gameover) {
+    if (cells[parseInt(item.id)].bee) {
+      document.getElementById(item.id).classList.add("attacked");
+      document.getElementById('restart').classList.add('active');
+      gameover = true;
+      revealAll();
+    } else
+      cells[parseInt(item.id)].reveal();
+  }
 }
 
 
@@ -47,5 +59,17 @@ function revealAll() {
     for (let j = 0; j < cols; j++) {
       cells[(i * cols) + j].reveal();
     }
+  }
+}
+
+function restart() {
+  if(gameover){
+    document.getElementById('restart').classList.remove('active');
+    node.innerHTML = null;
+    options = [];
+    beesCount = 20;
+    cells = [];
+    gameover = false;
+    setup();
   }
 }
